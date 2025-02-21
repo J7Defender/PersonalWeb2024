8d5133687a91169f8cbc4b1023ef0808638ce760
Duy Pham <72265557+J7Defender@users.noreply.github.com>
2025-02-22 02:23:17 +0700

    Add shorten content field for noteModel

--- models/noteModel.js
+++ models/noteModel.js
@@ -3,11 +3,15 @@ import mongoose from "mongoose";
 const noteSchema = mongoose.Schema({
   title: {
     type: String,
+    default: "New Note", // Default value for content
     required: true,
   },
   content: {
     type: String, // Maximum length of 16 Megabytes
   },
+  shorten: {
+    type: String, // Maximum length of 16 Megabytes
+  }
 });
 
 // TODO: Implement for storing much longer text using GridFS
